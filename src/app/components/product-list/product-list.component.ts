8ea41e773734261fe2390fdd2736e4b67af6c066
Alexander Verkhoturkin <verhoturkin@gmail.com>
2022-10-30 20:01:52 +0500

    add product categories routing

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../services/product.service";
 import { Product } from "../../common/product";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,26 @@ import { Product } from "../../common/product";
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    this.listProduct();
+    this.route.paramMap.subscribe(() => this.listProduct());
   }
 
   private listProduct() {
-    this.productService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
